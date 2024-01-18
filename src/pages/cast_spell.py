fa12ea657183d97f4726a29b682389fdfc9ffe93
achrafib1 <achibbuisness512@gmail.com>
2024-01-18 13:52:23 +0100

    Refactor cast_spell.py
    - Add plot for predicted probabilities of digits

--- src/pages/cast_spell.py
+++ src/pages/cast_spell.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 from streamlit_drawable_canvas import st_canvas
+import numpy as np
+import time
 
 
 def show():
@@ -11,7 +13,7 @@ def show():
         st.title("Here you can cast your spell!")
 
     # Load the crystal ball image
-    crystal_ball = Image.open("static/images/digit_ball.jpg").resize((1000, 1250))
+    crystal_ball = Image.open("static/images/digit_ball.jpg").resize((1000, 1850))
 
     # Create a draw object
     draw = ImageDraw.Draw(crystal_ball)
@@ -21,20 +23,23 @@ def show():
     color = "rgb(0, 0, 0)"  #  color
 
     # Create columns for the canvas and the prediction zone
-    col1, col2 = st.columns(2, gap="large")
+    col1, col2, col3 = st.columns(3, gap="large")
 
     # In the first column, create the canvas
     with col1:
         st.title("Cast Your Spell")
-        canvas = st_canvas()
+        canvas = st_canvas(height=350)
+    with col3:
+        st.title("Predicted Probabilities")
+        progress_plot = st.empty()
+        probabilities = [0 for _ in range(10)]
+        progress_plot.bar_chart(probabilities, use_container_width=True, height=350)
 
-    # Below the canvas, create the 'Predict' button
     if st.button("Predict"):
         # Get the image from the canvas
         image = canvas.image_data
 
         # Preprocess the image and make a prediction
-        # For simplicity, let's say the predicted digit is 7
         digit = 1
 
         # Position where the digit will be drawn
@@ -43,8 +48,17 @@ def show():
         _, _, textwidth, textheight = draw.textbbox((0, 0), text, font=font)
         position = ((width - textwidth) / 2, (height - textheight) / 2)
         st.write(position)
+
         # Draw the digit on the crystal ball image
         draw.text(position, text, fill=color, font=font)
+
+        probabilities = np.random.rand(10)
+        for i in range(9):  # 8 steps of 12.5% each
+            progress_plot.bar_chart(
+                [min((i) / 8, 1) * probabilities[j] for j in range(10)],
+                use_container_width=True,
+            )
+        time.sleep(0.2)  #
 
     # In the second column, display the crystal ball image
     with col2:
